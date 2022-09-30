bba9ca6ecd5127a65fa32178e6db156fe44ebe39
[l30l] <[847816474@qq.com]>
2022-09-30 18:27:00 +0800

    fix: 修复了登录后跳转路由冗余的问题

--- src/router/index.js
+++ src/router/index.js
@@ -3,19 +3,19 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-  { path: '/', redirect: '/login' },
+export const constantRoutes = [
   {
     path: '/login',
     component: () => import('@/views/login/index.vue'),
     hidden: true
-  }
+  },
+  { path: '/', redirect: '/dashboard' }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: constantRoutes
 })
 
 export default router
